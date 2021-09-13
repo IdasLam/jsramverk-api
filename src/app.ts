46ec95b7c75691cc1352aada69c9b81c5630e3fe
Ida Lam <idaslam@outlook.com>
2021-09-13 15:53:23 +0200

    seperated the app

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import express from "express"
-import cors from 'cors'
-import morgan from "morgan"
-import {error, errorRes} from './middleware/error'
+import app from './express'
 
-import {index} from './routes/index'
-import router from './routes/document'
 
-const app = express();
 const port = process.env.PORT || 1337;
-
-app.use(cors());
-app.use(express.json())
-
-if (process.env.NODE_ENV !== 'test') {
-    // use morgan to log at command line
-    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
-}
-
-app.use('/', index);
-app.use('/document', router);
-
-
-app.use(error)
-app.use(errorRes)
 
 
 // Start up server
---
+++ src/express.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from 'cors'
+import morgan from "morgan"
+import {error, errorRes} from './middleware/error'
+
+import {index} from './routes/index'
+import router from './routes/document'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+app.use('/', index);
+app.use('/document', router);
+
+
+app.use(error)
+app.use(errorRes)
+
+export default app
