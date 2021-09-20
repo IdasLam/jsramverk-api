9ac32393e094104e78c2b56ce9df1af1861f4496
Ida Lam <idaslam@outlook.com>
2021-09-20 14:16:21 +0200

    implemented sockets.io, save document each time it changes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,37 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import app from './express'
+import { Server } from 'socket.io'
+import * as document from './database/models/documents'
 
+type Doc = {
+    _id: string
+    title: string
+    content: string
+}
 
 const port = process.env.PORT || 1337;
 
-
 // Start up server
-app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000", "https://www.student.bth.se"],
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    socket.on('create', async (id) => {
+        socket.join(id);
+
+        socket.emit('doc', await document.findDocument(id))
+    });
+
+    socket.on('updatedDoc', (doc: Doc) => {
+        socket.to(doc._id).emit('doc', doc)
+
+        document.saveDocument(doc)
+    })
+});
