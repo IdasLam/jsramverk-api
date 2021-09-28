0bdced82583d2a420acf5a4823dce8a4d8bbe0cf
Ida Lam <idaslam@outlook.com>
2021-09-28 20:37:48 +0200

    configs to get cookies to work

--- src/express.ts
+++ src/express.ts
@@ -5,13 +5,20 @@ import {error, errorRes} from './middleware/error'
 import {index} from './routes/index'
 import router from './routes/document'
 import login from './routes/login'
+import cookieParser from 'cookie-parser'
 
 import verifyToken from './middleware/jwt'
 
 const app = express()
 
-app.use(cors())
+app.enable('trust proxy') 
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 if (process.env.NODE_ENV !== 'test') {
     // use morgan to log at command line
