61a8f0c3ea79f278fc8873cc0f403e36155b5bf9
Ida Lam <idaslam@outlook.com>
2021-09-28 20:38:05 +0200

    added socket endpoints

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ type Doc = {
     _id: string
     title: string
     content: string
+    access: string[]
 }
 
 const port = process.env.PORT || 1337;
@@ -24,23 +25,32 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-    socket.on('create', async ({id, username}) => {
+    socket.on('create', async ({id}) => {
         if (!id) return
 
         socket.join(id);
 
-        socket.emit('doc', await document.findDocument(id, username))
+        socket.emit('doc', await document.findDocument(id))
     });
 
     socket.on('updatedDoc', (doc: Doc) => {
         socket.to(doc._id).emit('doc', doc)
 
         document.saveDocument(doc)
+    })
 
+    socket.on('updatedAcess', async (doc: Doc) => {
+        io.in(doc._id).emit('doc', doc) 
+        
+        document.addDocumentAccess(doc._id, doc.access)
     })
 
     socket.on('close', () => {
         socket.disconnect()
+    })
+
+    socket.on('leave', (id: string) => {
+        socket.leave(id)
     })
 });
 
