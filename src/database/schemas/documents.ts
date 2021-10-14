382dd3eaf2b6410f5266484126cff29128bc0590
Ida Lam <idaslam@outlook.com>
2021-10-14 17:25:53 +0200

    added new columns in schema

--- src/database/schemas/documents.ts
+++ src/database/schemas/documents.ts
@@ -3,13 +3,17 @@ import { Schema } from "mongoose"
 type Doc = {
     title: string,
     content: string,
-    access: string[]
+    access: string[],
+    type: 'code' | 'text',
+    code: string
 }
 
 const docSchema = new Schema<Doc>({
     title: { type: String},
     content: { type: String},
-    access: [{type: String}]
+    access: [{type: String}],
+    type: {default: 'text', type: String},
+    code: {type: String, default: ''}
 });
 
 export default docSchema;
