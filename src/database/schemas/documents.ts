9e12fa04fcb45b53df21906784ba0b29aab5fa9e
Ida Lam <idaslam@outlook.com>
2021-10-20 16:12:29 +0200

    added comments to doc

--- src/database/schemas/documents.ts
+++ src/database/schemas/documents.ts
@@ -1,11 +1,21 @@
 import { Schema } from "mongoose"
 
-type Doc = {
+export type Comment = {
+    selected: string,
+    y: number,
+    comment: string,
+    user: string,
+    date: string,
+    _id?: string
+}
+
+export type Doc = {
     title: string,
     content: string,
     access: string[],
     type: 'code' | 'text',
     code: string
+    comments?: Comment[]
 }
 
 const docSchema = new Schema<Doc>({
@@ -13,7 +23,14 @@ const docSchema = new Schema<Doc>({
     content: { type: String},
     access: [{type: String}],
     type: {default: 'text', type: String},
-    code: {type: String, default: ''}
+    code: {type: String, default: ''},
+    comments: [{
+        selected: String,
+        y: Number,
+        comment: String,
+        user: String,
+        date: String
+    }]
 });
 
 export default docSchema;
