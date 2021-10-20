3a692edfddec165925777c1151e479435dfa450d
Ida Lam <idaslam@outlook.com>
2021-10-20 16:12:59 +0200

    when saving document, clear comments that have been removed

--- src/database/models/documents.ts
+++ src/database/models/documents.ts
@@ -1,4 +1,4 @@
-import { Document } from 'mongoose'
+import { Comment, Doc } from '../schemas/documents'
 import DB from '../schemas/index'
 
 export type DocumentType = {
@@ -8,12 +8,27 @@ export type DocumentType = {
     access?: string[]
     type?: 'code' | 'text'
     code: string
+    comments?: Comment[]
 }
 
 export const findDocument = async (_id: string): Promise<DocumentType> => {
     const {Documents} = await DB
 
     return await Documents.findOne({_id}).lean()
+}
+
+export const saveComment = async (_id: string, comment: Comment) => {
+    const {Documents} = await DB
+
+    await Documents.updateOne({_id}, {
+        $push: {
+            comments: comment
+        }
+    })
+
+    const allComment = (await findDocument(_id)).comments
+
+    return allComment[allComment.length - 1]._id
 }
 
 
@@ -24,6 +39,15 @@ export const saveDocument = async ({_id, title, content, code}: DocumentType) =>
         title,
         content,
         code
+    })
+
+    const doc = await findDocument(_id)
+    const allComment = doc.comments
+
+    const filteredComments = allComment.filter(comment => doc.content.includes(comment?._id))
+
+    await Documents.updateOne({_id}, {
+        comments: filteredComments
     })
 }
 
